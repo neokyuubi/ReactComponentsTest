71e748b90c85868fba84daeceafeec357d881743
Neokyuubi <neokyuubi@gmail.com>
2023-03-01 08:19:23 +0100

    Composabel

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const html = 
-(
-  <>
-    <img src="./react-logo.png" width="40px"/>
-    <h1>Func facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Neokyuubi is starting to use it</li>
-    </ul>
-  </>
-);
+function FirstComposabel()
+{
+  return (
+    <>
+      <img src="./react-logo.png" width="40px"/>
+      <h1>Func facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Neokyuubi is starting to use it</li>
+      </ul>
+    </>
+  );
+}
 
-ReactDOM.render(html, document.getElementById("root"));
+ReactDOM.render(<FirstComposabel/>, document.getElementById("root"));
